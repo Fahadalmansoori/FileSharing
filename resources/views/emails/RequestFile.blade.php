487ebc6c0b64ee74c6204d212693195009fb0018
Fahed <u1578102@unimail.hud.ac.uk>
2019-04-19 23:38:44 +0100

    Added Link to Academic integrity in the email template

--- resources/views/emails/RequestFile.blade.php
+++ resources/views/emails/RequestFile.blade.php
@@ -1,8 +1,14 @@
 @component('mail::message')
 Hello {{$user->getFullName()}}
 
- Your have requested to  download the  assignment  <span class="text-info"> {{$file->title}} </span>. please click on the link below to download it.
 
+ Your have requested to  download the  assignment  <span class="text-info"> {{$file->title}} </span>. please click on the link below to download it.
+ <div class="alert alert-warning"> <p class="text-center alert alert-warning"> Please use the assignment as a reference and dont breach the academic integrity  </p>
+<p> <a  href="https://www.hud.ac.uk/registry/regulations-and-policies/studentregs/" class=" btn btn-outline-wraning">  Read the Academic Integrity </a>
+
+
+</p>
+</div>
 @component('mail::button', ['url' => route('upload.download', $file)])
 Download assingnment
 @endcomponent
@@ -10,3 +16,6 @@ Download assingnment
 Thanks,<br>
 {{ config('app.name') }}
 @endcomponent
+
+
+
