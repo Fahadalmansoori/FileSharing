be4b224502eb293364fda135bd95c97fc0e8cd71
Fahed <u1578102@unimail.hud.ac.uk>
2019-05-01 00:49:17 +0100

    Fix the nav bar obile view

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -1,9 +1,9 @@
-<nav class="navbar navbar-expand-md  bg-dark border mb-5 p-4 border">
+<nav class="navbar navbar-expand-md  navbar-dark bg-dark border mb-5 p-4 border">
 
     <a class="navbar-brand" href="{{ url('/') }}">
-        {{ config('app.name', 'File sharing') }}
+         {{ config('app.name', 'File sharing') }}
     </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+    <button class="navbar-toggler border text-info" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
         <span class="navbar-toggler-icon"></span>
     </button>
 
@@ -12,24 +12,24 @@
 @auth
         <ul class="nav nav-pills  m-auto">
             <li class="nav-item m-2">
-                <a class="nav-link border {{request()->is('account')? 'active' : ' '}}" href="{{ route('account.index') }}"> My Dashboard</a>
+                <a class="nav-link border text-info {{request()->is('account')? 'active' : ' '}}" href="{{ route('account.index') }}"> My Dashboard</a>
             </li>
             <li class="nav-item m-2">
-                <a class="nav-link border {{request()->is('school/courses/*')? 'active' : ' '}}" href="{{ route('school.page',auth()->user()->school) }}"> My School</a>
+                <a class="nav-link bordertext-info border text-info {{request()->is('school/courses/*')? 'active' : ' '}}" href="{{ route('school.page',auth()->user()->school) }}"> My School</a>
             </li>
             <li class="nav-item m-2">
-                <a class="nav-link border {{request()->is('school')? 'active' : ' '}}" href="{{ route('course.page', auth()->user()->course) }}"> My Course</a>
+                <a class="nav-link border text-info {{request()->is('school')? 'active' : ' '}}" href="{{ route('course.page', auth()->user()->course) }}"> My Course</a>
             </li>
             @if(auth()->user()->hasRole('Tutor'))
             <li class="nav-item dropdown border m-2 ">
-                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Courses</a>
+                <a class="nav-link dropdown-toggle text-info" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Courses</a>
                 <div class="dropdown-menu">
                     <a class="dropdown-item border-bottom " href="{{route('course.index')}}">All courses</a>
 
                 </div>
             </li>
             <li class="nav-item dropdown border m-2">
-                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Modules</a>
+                <a class="nav-link dropdown-toggle text-info" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Modules</a>
                 <div class="dropdown-menu">
                     <a class="dropdown-item" href="{{route('module.index')}}">All Modules</a>
                     <a class="dropdown-item" href="{{route('superAdmin.index',auth()->user())}}"> Admin Dashboard</a>
@@ -61,7 +61,7 @@
 
 @if(auth()->user()->hasRole('Tutor'))
             <li class="nav-item dropdown m-2 mr-3">
-                <a class="nav-link dropdown-toggle border" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Tutor Tools</a>
+                <a class="nav-link dropdown-toggle border text-info" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Tutor Tools</a>
                 <div class="dropdown-menu">
                 <a class="dropdown-item border" href="{{route('admin.index')}}">Tutor Dashboard</a>
                     <a class="dropdown-item " href="#">Pending assignments</a>
@@ -73,7 +73,7 @@
     @endif
     @if(auth()->user()->hasRole('Super_Admin') )
             <li class="nav-item dropdown m-2 mr-3">
-                <a class="nav-link dropdown-toggle border" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Admin Tools</a>
+                <a class="nav-link dropdown-toggle border text-info" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Admin Tools</a>
                 <div class="dropdown-menu">
                 <a class="dropdown-item border" href="{{route('superAdmin.index',auth()->user())}}">Super Admin Dashboard</a>
 
@@ -83,8 +83,8 @@
             </li>
     @endif
 
-            <li class="nav-item dropdown">
-                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+            <li class="nav-item dropdown m-2 mr-3 ">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle  text-info" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                     {{ Auth::user()->first_name }} {{ Auth::user()->student_number }} <span class="caret"></span>
 
 
